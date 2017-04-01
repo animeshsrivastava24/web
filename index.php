102557152233403e2451d496303916ae9b5ca49a
rahul <rahul10mail@gmail.com>
2017-04-01 19:39:56 +0530

    pages

---
+++ index.php
@@ -0,0 +1,181 @@
+<!DOCTYPE HTML>
+<!--
+	Alpha by HTML5 UP
+	html5up.net | @ajlkn
+	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
+-->
+<html>
+	<head>
+		<title>Home IRIC</title>
+		<meta charset="utf-8" />
+		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<!--[if lte IE 8]><script src="assets/js/ie/html5shiv.js"></script><![endif]-->
+		<link rel="stylesheet" href="assets/css/main.css" />
+		<!--[if lte IE 8]><link rel="stylesheet" href="assets/css/ie8.css" /><![endif]-->
+	</head>
+	<body class="landing">
+		<div id="page-wrapper">
+
+			<!-- Header -->
+				<header id="header" class="alt">
+					<h1><a href="index.php">IRIC</a></h1>
+					<nav id="nav">
+						<ul>
+							<li><a href="index.php">Home</a></li>
+							<li><a href="vision.html" >Vision</a></li>
+							<li>
+								<a href="#" class="icon fa-angle-down">People</a>
+								<ul>
+									<li><a href="faculty.html">Faculty</a></li>
+									<li><a href="members.html">Members</a></li>
+									<!--li><a href="elements.html">Elements</a></li-->
+									<!--li>
+										<a href="#">Submenu</a>
+										<ul>
+											<li><a href="#">Option One</a></li>
+											<li><a href="#">Option Two</a></li>
+											<li><a href="#">Option Three</a></li>
+											<li><a href="#">Option Four</a></li>
+										</ul>
+									</li-->
+								</ul>
+							</li>
+							<li><a href="events.html" >Events</a></li>
+							<li><a href="contact.html" >Contact</a></li>
+							<li><a href="#" class="button">Sign Up</a></li>
+						</ul>
+					</nav>
+				</header>
+
+			<!-- Banner -->
+				<section id="banner">
+					<h2>IRIC</h2>
+					<p>Innovative Research Incubation Club</p>
+					<p> ... A part of Incubation Center NITH</p>
+					<ul class="actions">
+						<li><a href="#" class="button special">Sign Up</a></li>
+						<li><a href="#" class="button">Learn More</a></li>
+					</ul>
+				</section>
+
+			<!-- Main -->
+				<section id="main" class="container">
+
+					<section class="box special">
+						<header class="major">
+							<h2>Official Research & Incubation Center
+							<br />
+							NIT HAMIRPUR</h2>
+							<p>A thousand mile journey starts with a single step.</p>
+						</header>
+						<!--span class="image featured"><img src="images/pic01.jpg" alt="" /></span-->
+					</section>
+
+					<section class="box special features">
+						<div class="features-row">
+							<section>
+								<!--span class="icon major fa-bolt accent2"></span-->
+								<img src="icons/rupee.png" alt="funding"></img>
+								<h3>Funding</h3>
+								<p>Giving Required Funding to Project Proposals.</p>
+							</section>
+							<section>
+								<!--span class="icon major fa-area-chart accent3"></span-->
+								<img src="icons/lighthouse.png" alt="funding"></img>
+								<h3>Guidance</h3>
+								<p>Get along with faculty in their projects.</p>
+							</section>
+						</div>
+						<div class="features-row">
+							<section>
+								<!--span class="icon major fa-cloud accent4"></span-->
+								<img src="icons/compose.png" alt="funding"></img>
+								<h3>Research Paper & Patent</h3>
+								<p>The main vision behind this club is to foster a research environment in college and converting ideas into research publications and patents.</p>
+							</section>
+							<section>
+								<!--span class="icon major fa-lock accent5"></span-->
+								<img src="icons/student.png" alt=""></img>
+								<h3>Internship</h3>
+								<p>Utilise your time by doing Internship work in the college only.</p>
+							</section>
+						</div>
+					</section>
+
+					<div class="row">
+						<div class="6u 12u(narrower)">
+
+							<section class="box special">
+								<span class="image featured"><img src="images/idea.jpg" alt="" /></span>
+								<h3>Project Proposal</h3>
+								<p>The link will be shortly opened.</p>
+								<ul class="actions">
+									<li><a href="#" class="button alt">Learn More</a></li>
+								</ul>
+							</section>
+
+						</div>
+						<div class="6u 12u(narrower)">
+
+							<section class="box special">
+								<span class="image featured"><img src="images/work.jpg" alt="" /></span>
+								<h3>Internship Opportunities</h3>
+								<p>We are in talks with esteemed faculties.</p>
+								<ul class="actions">
+									<li><a href="#" class="button alt">Learn More</a></li>
+								</ul>
+							</section>
+
+						</div>
+					</div>
+
+				</section>
+
+			<!-- CTA -->
+				<!--section id="cta">
+
+					<h2>Sign up for beta access</h2>
+					<p>Blandit varius ut praesent nascetur eu penatibus nisi risus faucibus nunc.</p>
+
+					<form>
+						<div class="row uniform 50%">
+							<div class="8u 12u(mobilep)">
+								<input type="email" name="email" id="email" placeholder="Email Address" />
+							</div>
+							<div class="4u 12u(mobilep)">
+								<input type="submit" value="Sign Up" class="fit" />
+							</div>
+						</div>
+					</form>
+
+				</section-->
+
+			<!-- Footer -->
+				<footer id="footer">
+					<ul class="icons">
+						<!--li><a href="#" class="icon fa-twitter"><span class="label">Twitter</span></a></li-->
+						<li><a href="http://facebook.com/iric.nith/" class="icon fa-facebook"><span class="label">Facebook</span></a></li>
+						<!--li><a href="#" class="icon fa-instagram"><span class="label">Instagram</span></a></li-->
+						<!--li><a href="#" class="icon fa-github"><span class="label">Github</span></a></li-->
+						<!--li><a href="#" class="icon fa-dribbble"><span class="label">Dribbble</span></a></li>
+						<li><a href="#" class="icon fa-google-plus"><span class="label">Google+</span></a></li-->
+					</ul>
+					<ul class="copyright">
+						<li>&copy;All rights reserved.</li><li>Design: <a href="http://html5up.net">HTML5 UP</a></li>
+						<li>Images: <a href="http://flaticon.com" >FLATICON</a>, <a href="http://unsplash.com" >UNSPLASH</a></li>
+					</ul>
+				</footer>
+
+		</div>
+
+		<!-- Scripts -->
+			<script src="assets/js/jquery.min.js"></script>
+			<script src="assets/js/jquery.dropotron.min.js"></script>
+			<script src="assets/js/jquery.scrollgress.min.js"></script>
+			<script src="assets/js/skel.min.js"></script>
+			<script src="assets/js/util.js"></script>
+			<!--[if lte IE 8]><script src="assets/js/ie/respond.min.js"></script><![endif]-->
+			<script src="assets/js/main.js"></script>
+
+	</body>
+</html>
